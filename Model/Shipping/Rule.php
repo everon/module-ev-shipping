f3bf5e85752d42ba7503d54ab08d50ce93eace5a
ec-m2demo container <container@ec-m2demo>
2017-03-24 20:49:04 +0000

    Added model and example CSV

---
+++ Model/Shipping/Rule.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace EdmondsCommerce\Shipping\Model\Shipping;
+
+use Magento\Framework\Model\AbstractModel;
+
+/**
+ * Class Rule
+ * @package EdmondsCommerce\Shipping\Model\Shipping
+ * Shipping Rule
+ */
+class Rule extends AbstractModel
+{
+
+}
---
+++ Test/Integration/IntegrationTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace EdmondsCommerce\Shipping\Test\Unit;
+use \PHPUnit_Framework_TestCase;
+
+
+abstract class IntegrationTestCase extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+}
