7ed3fa2aff7e1e9574179e754ca191f87858fc0a
ec-m2demo container <container@ec-m2demo>
2017-03-26 01:02:21 +0000

    Added config

---
+++ Model/Carrier/Shipping.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace EdmondsCommerce\Shipping\Model\Carrier;
+
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Shipping\Model\Carrier\AbstractCarrier;
+use Magento\Shipping\Model\Carrier\CarrierInterface;
+
+class Shipping extends AbstractCarrier implements CarrierInterface
+{
+
+    protected $_code = 'ecshipping';
+
+    /**
+     * Collect and get rates
+     *
+     * @param RateRequest $request
+     * @return \Magento\Framework\DataObject|bool|null
+     * @api
+     */
+    public function collectRates(RateRequest $request)
+    {
+        // TODO: Implement collectRates() method.
+    }
+
+    /**
+     * Get allowed shipping methods
+     *
+     * @return array
+     * @api
+     */
+    public function getAllowedMethods()
+    {
+        // TODO: Implement getAllowedMethods() method.
+    }
+}
