c5f6c1575fd8478e746ab0ac38c7e41a2d2acc8b
ec-m2demo container <container@ec-m2demo>
2017-03-28 21:17:54 +0000

    Added custom file backend model to handle import and added comments

--- Model/Upload/Importer.php
+++ Model/Upload/Importer.php
@@ -4,5 +4,18 @@ namespace EdmondsCommerce\Shipping\Model\Upload;
 
 class Importer
 {
+    /**
+     * @var Validator
+     */
+    private $validator;
 
+    public function __construct(Validator $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function import()
+    {
+
+    }
 }
