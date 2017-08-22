004f4155252ff279db11be9fce6424a6b7a50988
ec-m2demo container <container@ec-m2demo>
2017-08-22 22:52:42 +0000

    Udpated tests

---
+++ Test/Unit/Model/Filter/PostcodeFilterTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace EdmondsCommerce\Shipping\Test\Unit\Model\Filter;
+
+use EdmondsCommerce\Shipping\Model\Filter\Postcode;
+
+class PostcodeFilterTest extends AbstractFilterTest {
+	/** @var Postcode */
+	protected $class;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->class = new Postcode();
+	}
+
+	/**
+	 * @param $requestedPostCode
+	 *
+	 * @return \Mockery\MockInterface
+	 */
+	protected function getPostCodeRequestMock( $requestedPostCode ) {
+		return $this->getRateRequestMock()->shouldReceive( 'getDestPostcode' )
+		            ->andReturn( $requestedPostCode )->getMock();
+	}
+
+	/**
+	 * @param array $patterns
+	 *
+	 * @return \Mockery\MockInterface
+	 */
+	protected function getPostCodeRateMock( array $patterns ) {
+		return $this->getRateMock()->shouldReceive( 'getPostCodes' )
+		            ->andReturn( $patterns )->getMock();
+	}
+
+	/**
+	 * @test
+	 */
+	public function itCanFilterOutWhenNoPostCodesMatch() {
+		$request = $this->getPostCodeRequestMock( 'BD17 7DB' );
+		$rate    = $this->getPostCodeRateMock( [ 'LS', 'NW1' ] );
+
+		$this->assertFalse( $this->class->filter( $request, $rate ) );
+	}
+
+	/**
+	 * @test
+	 */
+	public function itCanPassOnPartialMatches() {
+		$request = $this->getPostCodeRequestMock( 'BD17 7DB' );
+		$rate    = $this->getPostCodeRateMock( [ 'BD', 'LS1' ] );
+
+		$this->assertTrue( $this->class->filter( $request, $rate ) );
+	}
+
+	public function itCanPassOnFullMatches()
+	{
+		$request = $this->getPostCodeRequestMock('BD17 7DB');
+		$rate = $this->getPostCodeRateMock(['LS%', 'NW2', 'BD17 7DB']);
+
+		$this->assertTrue($this->class->filter($request, $rate));
+	}
+}
---
+++ Test/Unit/Model/Filter/WebsiteFilterTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace EdmondsCommerce\Shipping\Test\Unit\Model\Filter;
+
+use EdmondsCommerce\Shipping\Model\Filter\Website;
+use Mockery\MockInterface;
+
+class WebsiteFilterTest extends AbstractFilterTest {
+	/** @var  Website */
+	protected $class;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->class = new Website();
+	}
+
+	/**
+	 * @param string $website
+	 *
+	 * @return MockInterface
+	 */
+	protected function getWebsiteRequestMock( $website ) {
+		return $this->getRateRequestMock()->shouldReceive( 'getWebsiteId' )->andReturn( $website )->getMock();
+	}
+
+	/**
+	 * @param string[] $websites
+	 *
+	 * @return MockInterface
+	 */
+	protected function getWebsiteRateMock( array $websites ) {
+		return $this->getRateMock()->shouldReceive( 'getWebsiteIds' )->andReturn( $websites )->getMock();
+	}
+
+	/**
+	 * @test
+	 */
+	public function itWillFilterOutAWebsite() {
+		$request = $this->getWebsiteRequestMock( '1' );
+		$rate    = $this->getWebsiteRateMock( [ '2', '3' ] );
+
+		$this->assertFalse( $this->class->filter( $request, $rate ) );
+	}
+
+	/**
+	 * @test
+	 */
+	public function itWillFindAWebsite() {
+		$request = $this->getWebsiteRequestMock( '2' );
+		$rate    = $this->getWebsiteRateMock( [ '1', '2' ] );
+
+		$this->assertTrue( $this->class->filter( $request, $rate ) );
+	}
+
+}
