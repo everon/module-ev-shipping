e237c198849b42adc913c6330c85dadef0b708a1
ec-m2demo container <container@ec-m2demo>
2017-09-08 20:31:22 +0000

    Cleaned up the rate writer to use M2 classes

--- Model/Config/Backend/Import/Writer.php
+++ Model/Config/Backend/Import/Writer.php
@@ -6,6 +6,10 @@ use Everon\EvShipping\Exception\RateImportException;
 use Everon\EvShipping\Exception\ShippingException;
 use Everon\EvShipping\Model\Rate\Validator;
 use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Exception\FileSystemException;
+use Magento\Framework\Filesystem\File\Read;
+use Magento\Framework\Filesystem\File\ReadInterfaceFactory;
+use Magento\Framework\Filesystem\File\WriteInterfaceFactory;
 
 class Writer
 {
@@ -13,21 +17,40 @@ class Writer
      * @var Validator
      */
     private $validator;
+
     /**
      * @var DirectoryList
      */
     private $directoryList;
 
     /**
+     * @var ReadInterface
+     */
+    private $readerFactory;
+
+    /**
+     * @var WriteInterface
+     */
+    private $writerFactory;
+
+    /**
      * Writer constructor.
      *
-     * @param Validator     $validator
-     * @param DirectoryList $directoryList
+     * @param Validator             $validator
+     * @param DirectoryList         $directoryList
+     * @param ReadInterfaceFactory  $readerFactory
+     * @param WriteInterfaceFactory $writerFactory
      */
-    public function __construct(Validator $validator, DirectoryList $directoryList)
-    {
+    public function __construct(
+        Validator $validator,
+        DirectoryList $directoryList,
+        ReadInterfaceFactory $readerFactory,
+        WriteInterfaceFactory $writerFactory
+    ) {
         $this->validator     = $validator;
         $this->directoryList = $directoryList;
+        $this->readerFactory = $readerFactory;
+        $this->writerFactory = $writerFactory;
     }
 
     /**
@@ -41,16 +64,17 @@ class Writer
      */
     public function handleImport($filePath)
     {
-        //Check file exists
-        if (!file_exists($filePath)) {
+        try {
+            //Try and open the file
+            /** @var Read $fileReader */
+            $fileReader = $this->readerFactory->create(['path' => $filePath]);
+        } catch (FileSystemException $e) {
+            //Could not find the file
             throw new RateImportException('Could not find the uploader rates file at: ' . $filePath);
         }
 
-        //Try and read the file
-        $data = @file_get_contents($filePath);
-        if ($data === false) {
-            throw new RateImportException('Could not read the file at ' . $filePath);
-        }
+        $data = $fileReader->readAll();
+        $fileReader->close();
 
         //Try and decode the file
         $json = json_decode($data, true);
@@ -64,7 +88,12 @@ class Writer
         //Passed all checks, write the file
         $targetPath = $this->directoryList->getPath('var') . '/shipping-config.json';
 
-        file_put_contents($targetPath, $data);
+        try {
+            $writer = $this->writerFactory->create(['path' => $targetPath]);
+            $writer->write($data);
+        } catch (FileSystemException $e) {
+            throw new RateImportException('Could not write rates file to: ' . $targetPath);
+        }
 
         return $targetPath;
     }
