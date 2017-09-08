8f60302e05e27b0a9aba070e8a98b17eaa11fa82
ec-m2demo container <container@ec-m2demo>
2017-09-08 21:11:40 +0000

    Added controller and menu item

---
+++ Controller/Adminhtml/Rates/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Everon\EvShipping\Controller\Adminhtml\Rates;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\App\ResponseInterface;
+
+class Index extends Action
+{
+
+    /**
+     * Dispatch request
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        
+    }
+}
